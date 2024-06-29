0320a8e233300a8d9fbd255deac9046ab7b5fc45
CHCHALLE <Chrischalle@yahoo.com>
2024-06-29 00:43:32 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
+  let pronoun = ["the", "out"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let newArray = [];
+  let word = "";
+  let website = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < adj.length; k++) {
+        word = pronoun[i] + adj[j] + noun[k] + ".com";
+        //debug.log(string)
+
+        newArray.push(word);
+      }
+    }
+  }
+
+  website = newArray.toString();
+  document.querySelector(".module").innerHTML = website;
+
   //write your code here
-  console.log("Hello Rigo from the console!");
+  return;
 };
